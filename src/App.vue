6d82a936b38f6e9956e614e2923592494da43726
Hunter <moontaser009@gmail.com>
2023-12-05 12:48:33 +0600

    added functionality and structure

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,47 @@
+
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <learning-survey @survey-submit="storeSurvey"></learning-survey>
+  <user-experiences :results="savedSurveyResults"></user-experiences>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LearningSurvey from './components/survey/LearningSurvey.vue';
+import UserExperiences from './components/survey/UserExperiences.vue';
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    LearningSurvey,
+    UserExperiences,
+  },
+  data() {
+    return {
+      savedSurveyResults: [],
+    };
+  },
+  methods: {
+    storeSurvey(surveyData) {
+      const surveyResult = {
+        name: surveyData.userName,
+        rating: surveyData.rating,
+        id: new Date().toString(),
+      };
+      this.savedSurveyResults.push(surveyResult);
+      console.log(surveyResult);
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
 }
 </style>
